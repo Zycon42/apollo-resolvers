c85ec5a1dd57cf83a6d0af9cc8d7c87fba16b7c2
Jan Dušek <jan.dusek90@gmail.com>
2018-03-10 03:04:28 +0100

    Update dist

--- dist/resolver.js
+++ dist/resolver.js
@@ -4,16 +4,16 @@ const promise_1 = require("./promise");
 const util_1 = require("./util");
 exports.createResolver = (resFn, errFn) => {
     const Promise = promise_1.getPromise();
-    const baseResolver = (root, args = {}, context = {}) => {
+    const baseResolver = (root, args = {}, context = {}, info = {}) => {
         // Return resolving promise with `null` if the resolver function param is not a function
         if (!util_1.isFunction(resFn))
             return Promise.resolve(null);
-        return util_1.Promisify(resFn)(root, args, context).catch(e => {
+        return util_1.Promisify(resFn)(root, args, context, info).catch(e => {
             // On error, check if there is an error handler.  If not, throw the original error
             if (!util_1.isFunction(errFn))
                 throw e;
             // Call the error handler.
-            return util_1.Promisify(errFn)(root, args, context, e).then(parsedError => {
+            return util_1.Promisify(errFn)(root, args, context, info, e).then(parsedError => {
                 // If it resolves, throw the resolving value or the original error.
                 throw parsedError || e;
             }, parsedError => {
@@ -24,26 +24,26 @@ exports.createResolver = (resFn, errFn) => {
     };
     baseResolver['createResolver'] = (cResFn, cErrFn) => {
         const Promise = promise_1.getPromise();
-        const childResFn = (root, args, context) => {
+        const childResFn = (root, args, context, info) => {
             // Start with either the parent resolver function or a no-op (returns null)
-            const entry = util_1.isFunction(resFn) ? util_1.Promisify(resFn)(root, args, context) : Promise.resolve(null);
+            const entry = util_1.isFunction(resFn) ? util_1.Promisify(resFn)(root, args, context, info) : Promise.resolve(null);
             return entry.then(r => {
                 // If the parent returns a value, continue
                 if (util_1.isNotNullOrUndefined(r))
                     return r;
                 // Call the child resolver function or a no-op (returns null)
-                return util_1.isFunction(cResFn) ? util_1.Promisify(cResFn)(root, args, context) : Promise.resolve(null);
+                return util_1.isFunction(cResFn) ? util_1.Promisify(cResFn)(root, args, context, info) : Promise.resolve(null);
             });
         };
-        const childErrFn = (root, args, context, err) => {
+        const childErrFn = (root, args, context, info, err) => {
             // Start with either the child error handler or a no-op (returns null)
-            const entry = util_1.isFunction(cErrFn) ? util_1.Promisify(cErrFn)(root, args, context, err) : Promise.resolve(null);
+            const entry = util_1.isFunction(cErrFn) ? util_1.Promisify(cErrFn)(root, args, context, info, err) : Promise.resolve(null);
             return entry.then(r => {
                 // If the child returns a value, throw it
                 if (util_1.isNotNullOrUndefined(r))
                     throw r;
                 // Call the parent error handler or a no-op (returns null)
-                return util_1.isFunction(errFn) ? util_1.Promisify(errFn)(root, args, context, err).then(e => {
+                return util_1.isFunction(errFn) ? util_1.Promisify(errFn)(root, args, context, info, err).then(e => {
                     // If it resolves, throw the resolving value or the original error
                     throw e || err;
                 }, e => {
